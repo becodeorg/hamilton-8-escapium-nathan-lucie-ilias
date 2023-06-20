382df2127b7137becb5602fb64d7ba871f325fd6
[Lucie Monteiro] <[lucie.mtro@gmail.com]>
2023-06-20 10:37:59 +0200

    finition navbar

--- js/topNav.js
+++ js/topNav.js
@@ -4,3 +4,24 @@ burger.addEventListener('click', () => {
     menu.classList.toggle('active');
     burger.classList.toggle('active');
 })
+
+let links = document.getElementsByClassName('links_menu');
+
+
+for (let link of links){
+    link.addEventListener('click', () => {
+    for (let linkcolorbase of links){
+        linkcolorbase.style.color = '#CBCBCB';
+    }
+    link.style.color = 'white';
+    for (lignered of links){
+        lignered.nextElementSibling.style.opacity = '0'
+    }
+    link.nextElementSibling.style.opacity = '1'
+ });
+}
+
+
+
+
+
