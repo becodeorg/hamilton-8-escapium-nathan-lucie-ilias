2c237380940de0e1510e5b8591014ef5d947cb04
[Lucie Monteiro] <[lucie.mtro@gmail.com]>
2023-06-26 11:04:44 +0200

    nav bar finie

--- js/topNav.js
+++ js/topNav.js
@@ -1,5 +1,20 @@
 
 function nav() {
+    const two_span = document.querySelector('.two_span')
+    const btn_room_nav = document.querySelector(".plus");
+    const list_room_nav = document.querySelector(".accordeon_nav_rooms");
+    const span_none = document.querySelector(".span_none");
+    btn_room_nav.addEventListener("click", () => {
+        list_room_nav.classList.toggle("accordeon_nav");
+        span_none.classList.toggle("display_span_nav");
+    })
+    const btn_blog_nav = document.querySelector(".plus2");
+    const list_blog_nav = document.querySelector(".accordeon_nav_blog");
+    const span_none2 = document.querySelector(".span_none2");
+    btn_blog_nav.addEventListener("click", () => {
+        list_blog_nav.classList.toggle("accordeon_nav2");
+        span_none2.classList.toggle("display_span_nav2");
+    })
     const burger = document.getElementById('burger');
     let menu = document.getElementById('nav_links');
     burger.addEventListener('click', () => {
@@ -23,6 +38,8 @@ function nav() {
 }
 
 export {nav};
+
+
 
 
 
