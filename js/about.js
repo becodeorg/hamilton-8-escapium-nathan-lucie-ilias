a85ec3570eeeb636823483e05fd225bbeee7a87c
[Lucie Monteiro] <[lucie.mtro@gmail.com]>
2023-06-26 02:17:11 +0200

    about fait

--- js/about.js
+++ js/about.js
@@ -38,4 +38,61 @@ document.addEventListener("DOMContentLoaded", function() {
       });
     }
   });
-  
+
+  //faqs
+
+const btn1 = document.querySelector(".btn_about_one");
+const p1 = document.querySelector(".p_about_one");
+const span1 = document.querySelector(".span_accordeon_one")
+btn1.addEventListener("click", () => {
+    p1.classList.toggle("accordeon");
+    span1.classList.toggle("display")
+
+})
+
+const btn2 = document.querySelector(".btn_about_two");
+const p2 = document.querySelector(".p_about_two");
+const span2 = document.querySelector(".span_accordeon_two")
+btn2.addEventListener("click", () => {
+    p2.classList.toggle("accordeon");
+    span2.classList.toggle("display")
+
+})
+const btn3 = document.querySelector(".btn_about_three");
+const p3 = document.querySelector(".p_about_three");
+const span3 = document.querySelector(".span_accordeon_three")
+btn3.addEventListener("click", () => {
+    p3.classList.toggle("accordeon");
+    span3.classList.toggle("display")
+})
+const btn4 = document.querySelector(".btn_about_four");
+const p4 = document.querySelector(".p_about_four");
+const span4 = document.querySelector(".span_accordeon_four")
+btn4.addEventListener("click", () => {
+    p4.classList.toggle("accordeon");
+    span4.classList.toggle("display")
+
+})
+const btn5 = document.querySelector(".btn_about_five");
+const p5 = document.querySelector(".p_about_five");
+const span5 = document.querySelector(".span_accordeon_five")
+btn5.addEventListener("click", () => {
+    p5.classList.toggle("accordeon");
+    span5.classList.toggle("display")
+
+})
+const btn6 = document.querySelector(".btn_about_six");
+const p6 = document.querySelector(".p_about_six");
+const span6 = document.querySelector(".span_accordeon_six")
+btn6.addEventListener("click", () => {
+    p6.classList.toggle("accordeon");
+    span6.classList.toggle("display")
+
+})
+const btn7 = document.querySelector(".btn_about_seven");
+const p7 = document.querySelector(".p_about_seven");
+const span7 = document.querySelector(".span_accordeon_seven")
+btn7.addEventListener("click", () => {
+    p7.classList.toggle("accordeon");
+    span7.classList.toggle("display")
+})
